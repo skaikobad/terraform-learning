19b0df9c670cfafdf2b395f8e924741a70498dc9
Kaikobad <skaikobad@gmail.com>
2026-08-15 09:35:11 +0600

    modify bootstrap file

--- day3-remote-state/bootstrap/main.tf
+++ day3-remote-state/bootstrap/main.tf
@@ -63,12 +63,3 @@ resource "aws_dynamodb_table" "terraform_locks" {
     Purpose = "State Locking"
   }
 }
- 
-# Output the bucket name so we can use it in the next part
-output "state_bucket_name" {
-  value = aws_s3_bucket.terraform_state.bucket
-}
- 
-output "dynamodb_table_name" {
-  value = aws_dynamodb_table.terraform_locks.name
-}
