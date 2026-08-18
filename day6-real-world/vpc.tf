1c2c93a44d81cf7d081d245dfaa95e1feb9ec730
Kaikobad <skaikobad@gmail.com>
2026-08-18 22:17:05 +0600

    update AZ

--- day6-real-world/vpc.tf
+++ day6-real-world/vpc.tf
@@ -16,7 +16,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"  # 256 IPs
-  availability_zone       = "us-west-1a"
+  availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true            # Auto-assign public IPs
   tags = { Name = "public-subnet-1a" }
 }
@@ -24,7 +24,7 @@ resource "aws_subnet" "public_1" {
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-west-1b"
+  availability_zone       = "us-west-2b"
   map_public_ip_on_launch = true
   tags = { Name = "public-subnet-1b" }
 }
@@ -33,14 +33,14 @@ resource "aws_subnet" "public_2" {
 resource "aws_subnet" "private_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.10.0/24"
-  availability_zone = "us-west-1a"
+  availability_zone = "us-west-2a"
   tags = { Name = "private-subnet-1a" }
 }
  
 resource "aws_subnet" "private_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.11.0/24"
-  availability_zone = "us-west-1b"
+  availability_zone = "us-west-2b"
   tags = { Name = "private-subnet-1b" }
 }
  
